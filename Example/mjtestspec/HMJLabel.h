0ca9dbcfcd9de88bca715473aec42f70fe90514c
hemengjie <1095928802@qq.com>
2020-11-13 16:42:06 +0800

    create and add code

---
+++ Example/mjtestspec/HMJLabel.h
@@ -0,0 +1,17 @@
+//
+//  HMJLabel.h
+//  mjtestspec_Example
+//
+//  Created by scmios on 2020/11/13.
+//  Copyright © 2020 MJHee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMJLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/mjtestspec/HMJLabel.m
@@ -0,0 +1,21 @@
+//
+//  HMJLabel.m
+//  mjtestspec_Example
+//
+//  Created by scmios on 2020/11/13.
+//  Copyright © 2020 MJHee. All rights reserved.
+//
+
+#import "HMJLabel.h"
+
+@implementation HMJLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
